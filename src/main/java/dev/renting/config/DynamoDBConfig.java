2b9021c24cd583b20b4e14e220397b7cf95d344f
Toni <antonicv.88@gmail.com>
2025-06-08 20:04:04 +0200

    Refactor DynamoDB configuration by removing commented-out code and cleaning up formatting

--- src/main/java/dev/renting/config/DynamoDBConfig.java
+++ src/main/java/dev/renting/config/DynamoDBConfig.java
@@ -1,6 +1,5 @@
 package dev.renting.config;
 
-
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,51 +8,27 @@ import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
 import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
 
-
-
 @Configuration
 public class DynamoDBConfig {
 
-    /*@Value("${aws.accessKey}")
-    private String accessKey;
+    // Define the single table name as a constant
+    public static final String TABLE_NAME = "TablaUnicaDynamoDB3"; // This is the name of your single table
 
-    @Value("${aws.secretKey}")
-    private String secretKey;*/
-
-    //@Value("${aws.region}")
-    @Value("${aws.region:eu-central-1}")
+    @Value("${aws.region:eu-central-1}") // Default to eu-central-1 if not specified
     private String region;
 
     @Bean
     public DynamoDbClient dynamoDbClient() {
         return DynamoDbClient.builder()
-                .credentialsProvider(DefaultCredentialsProvider.create())
+                .credentialsProvider(DefaultCredentialsProvider.create()) // Use default credential provider chain
                 .region(Region.of(region))
                 .build();
     }
+
     @Bean
     public DynamoDbEnhancedClient dynamoDbEnhancedClient(DynamoDbClient dynamoDbClient) {
         return DynamoDbEnhancedClient.builder()
                 .dynamoDbClient(dynamoDbClient)
                 .build();
     }
-
-    /*@Bean
-    public DynamoDbClient dynamoDbClient() {
-        AwsBasicCredentials awsCreds = AwsBasicCredentials.create(accessKey, secretKey);
-
-        // Use DynamoDbClientBuilder instead of DynamoDbClient.Builder
-        DynamoDbClientBuilder builder = DynamoDbClient.builder()
-                .credentialsProvider(StaticCredentialsProvider.create(awsCreds))
-                .region(Region.of(region));
-
-        return builder.build();
-    }*/
-
-   /* @Bean
-    public DynamoDbEnhancedClient dynamoDbEnhancedClient(DynamoDbClient dynamoDbClient) {
-        return DynamoDbEnhancedClient.builder()
-                .dynamoDbClient(dynamoDbClient)
-                .build();
-    }*/
 }
